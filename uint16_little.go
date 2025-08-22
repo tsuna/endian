f32ff6e1a1324b0ae3a44ffbb3e5056011d57663
Nicolas Peugnet <nicolas.peugnet@lip6.fr>
2025-08-22 12:38:19 +0200

    Add loong64 and riscv64 support
    These two architectures are built at least by Debian so it makes sense
    to support them. Both are little endian as seen in the Go sources [1].
    
    [1] https://cs.opensource.google/go/go/+/master:src/encoding/binary/native_endian_little.go;l=5

--- uint16_little.go
+++ uint16_little.go
@@ -3,7 +3,7 @@
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the COPYING file.
 
-// +build 386 amd64 amd64p32 arm arm64 ppc64le mipsle mips64le mips64p32le
+// +build 386 amd64 amd64p32 arm arm64 ppc64le mipsle mips64le mips64p32le loong64 riscv64
 
 package endian
 
